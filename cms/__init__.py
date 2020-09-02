2b86d110af2b72d576121d1415ce3cbac321aa93
ancalita <27920906+ancalita@users.noreply.github.com>
2020-09-02 18:05:19 +0100

    Completed module

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, abort
-from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from cms.admin.models import Type, Content, Setting, User
+from cms.admin.models import Type, Content, Setting, User, db
+from cms.admin.__init__ import admin_bp
 
 ## Application Configuration
 app = Flask(__name__)
@@ -10,42 +9,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'b2de7FkqvkMyqzNFzxCkgnPKIGP6i4Rc'
 #!
 
-## Models
+db.init_app(app)
+app.register_blueprint(admin_bp)
 
-#!
-
-## Admin Routes
-def requested_type(type):
-    types = [row.name for row in Type.query.all()]
-    return True if type in types else False
-
-@app.route('/admin/', defaults={'type': 'page'})
-@app.route('/admin/<type>')
-def content(type):
-    if requested_type(type):
-        content = Content.query.join(Type).filter(Type.name == type)
-        return render_template('admin/content.html', type=type, content=content)
-    else:
-        abort(404)
-
-@app.route('/admin/create/<type>')
-def create(type):
-    if requested_type(type):
-        types = Type.query.all()
-        return render_template('admin/content_form.html', title='Create', types=types, type_name=type)
-    else:
-        abort(404)
-
-@app.route('/admin/users')
-def users():
-    users = User.query.all()
-    return render_template('admin/users.html', title='Users', users=users)
-
-@app.route('/admin/settings')
-def settings():
-    settings = Setting.query.all()
-    return render_template('admin/settings.html', title='Settings', settings=settings)
-#!
 
 ## Front-end Route
 @app.template_filter('pluralize')
